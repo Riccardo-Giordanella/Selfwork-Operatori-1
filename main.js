d19d2d2519a96f8912ef368963d10d33ef9b2d41
Riccardo Giordanella <it.gg@hotmail.it>
2024-08-02 23:00:52 +0200

    Fine esercizio

---
+++ main.js
@@ -0,0 +1,12 @@
+// Creazione delle 5 variabili numeriche
+let num1 = 1;
+let num2 = 5;
+let num3 = 10;
+let num4 = 15;
+let num5 = 20;
+// La console deve mostrare la somma di tutte le variabili:
+console.log(`La somma delle 5 variabili è:`, num1 + num2 + num3 + num4 + num5);
+// La console deve ora mostrare la media delle 5 variabili
+console.log(`La media delle 5 variabili è`, num1 + num2 + num3 + num4 + num5 / 5);
+
+// Fine esercizio
